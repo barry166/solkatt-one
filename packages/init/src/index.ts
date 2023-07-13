c395d327b2f3f706a585590040fa137c86b9b61e
zhuanghj <zhuanghj@landray.com.cn>
2023-07-14 00:06:15 +0800

    test: test

--- packages/init/src/index.ts
+++ packages/init/src/index.ts
@@ -1,8 +1,12 @@
 import { Command } from '@solkatt-one/models'
 
 class InitCommand extends Command {
+  projectName: string
+  force: boolean
   init() {
     console.log('init command init..', this.cmdName, this.cmdOption)
+    this.projectName = this.cmdName
+    this.force = this.cmdOption?.force
   }
 
   exec() {
