e63e4fc11103eb73f4ad3b3eb2d91cf71f6ffe65
barry166 <bzhuang166@gmail.com>
2023-07-14 15:59:25 +0800

    feat: vscode调试配置

--- packages/init/src/index.ts
+++ packages/init/src/index.ts
@@ -1,16 +1,42 @@
+import fs from 'node:fs'
 import { Command } from '@solkatt-one/models'
 
 class InitCommand extends Command {
   projectName: string
   force: boolean
+  cwd: string
   init() {
     console.log('init command init..', this.cmdName, this.cmdOption)
     this.projectName = this.cmdName
     this.force = this.cmdOption?.force
+    this.cwd = process.cwd() //当前操作目录
   }
 
-  exec() {
-    console.log('init command exec..')
+  async exec() {
+    // 准备阶段
+    await this.prepare()
+    // 下载模板到全局目录
+    await this.downloadTemplate()
+    // 安装模板到目标目录
+    await this.installTemplate()
+  }
+
+  async prepare() {
+    //todo
+    const res = await this.isDirEmpty()
+    console.log('res', res)
+  }
+
+  async downloadTemplate() {
+    //todo
+  }
+  async installTemplate() {
+    //todo
+  }
+
+  async isDirEmpty() {
+    const files = fs.readdirSync(this.cwd)
+    console.log('files', files)
   }
 }
 
